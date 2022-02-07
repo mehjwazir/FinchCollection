862041a23214d0598ce6267383cee4ee8e79731f
mehjwazir <mehjwazir@gmail.com>
2022-02-07 15:27:57 -0800

    finches lab 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,18 +1,33 @@
+from tabnanny import verbose
+from django.urls import reverse
 from django.db import models
 
 # Create your models here.
 
 
 class Finch(models.Model):
-    # define the fields/columns
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
 
+    class Meta:
+        verbose_name = 'finch'
+        verbose_name_plural = 'finches'
 
     def __str__(self):
- 	   return f'{self.name} ({self.id})'
+        return f'{self.name}, ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+
+# finches = [
+#   Finch('Ryland', 'saffron', 'chill, muchos siestas', 3),
+#   Finch('Mehj', 'blue', 'pretty bird, sips tea', 2),
+#   Finch('Dave', 'spice', 'spicy-boy, charming cat-collector, flex-box girrrrl', 4),
+#   Finch('Devlin', 'society', 'HIGH-society', 4),
+# ]
 
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Finch 
+from django.views.generic import CreateView, UpdateView, DeleteView
+from .models import Finch
 
 # Create your views here.
 
@@ -19,3 +20,21 @@ def finches_index(request):
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   return render(request, 'finches/detail.html', {'finch': finch})
+
+#Class-Based View (CBV)
+
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['type', 'description', 'age']
+
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'

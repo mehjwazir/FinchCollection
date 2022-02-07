54aa18c0fb265436aa09abec454d29d5a99e5825
mehjwazir <mehjwazir@gmail.com>
2022-02-07 14:42:35 -0800

    finish lab 2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Finch(models.Model):
+    # define the fields/columns
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+
+    def __str__(self):
+ 	   return f'{self.name} ({self.id})'
+
+
+
+
+# finches = [
+#     Finch('Ryland', 'saffron', 'chill, muchos siestas', 3),
+#     Finch('Mehj', 'blue', 'pretty bird, sips tea', 2),
+#     Finch('Dave', 'spice', 'spicy-boy, charming cat-collector, flex-box girrrrl', 4),
+#     Finch('Devlin', 'society', 'HIGH-society', 4),
+# ]
